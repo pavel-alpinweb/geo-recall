f5d357d6706af4a071e8cfa25ed96e9bb62abed1
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2018-09-08 13:04:09 +0600

    add place mark

--- src/map.js
+++ src/map.js
@@ -29,30 +29,26 @@ function init() {
     // Создание метки.
     function createPlacemark(coords) {
         return new ymaps.Placemark(coords, {
-            iconCaption: 'поиск...'
+            iconCaption: ''
         }, {
-            preset: 'islands#violetDotIconWithCaption',
-            draggable: true
+            preset: 'islands#violetDotIconWithCaption'
         });
     }
 
     // Определяем адрес по координатам (обратное геокодирование).
     function getAddress(coords) {
-        myPlacemark.properties.set('iconCaption', 'поиск...');
+        // myPlacemark.properties.set('iconCaption', 'поиск...');
         ymaps.geocode(coords).then(function (res) {
             var firstGeoObject = res.geoObjects.get(0);
-
             myPlacemark.properties
                 .set({
-                    // Формируем строку с данными об объекте.
-                    iconCaption: [
-                        // Название населенного пункта или вышестоящее административно-территориальное образование.
-                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-                    ].filter(Boolean).join(', '),
                     // В качестве контента балуна задаем строку с адресом объекта.
-                    balloonContent: firstGeoObject.getAddressLine()
+                    balloonContent: 
+                    `
+                    <div class="container">
+                        <h3 class="heading">${firstGeoObject.getAddressLine()}</h3>
+                    </div>
+                    `
                 });
         });
     }
